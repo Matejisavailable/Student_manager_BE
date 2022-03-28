afcc7dd4d420c165c182fc870328e63b778d55e5
MK <mat.majla@gmail.com>
2022-03-28 19:38:05 +0200

    zaciatok

---
+++ src/main/java/Mafia/StudentManager/StudentManagerApplication.java
@@ -0,0 +1,13 @@
+package Mafia.StudentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagerApplication.class, args);
+	}
+
+}
