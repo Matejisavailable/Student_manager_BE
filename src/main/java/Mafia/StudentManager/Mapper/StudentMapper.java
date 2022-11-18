44a84e1826d2175a7233384d026908a3a10759b7
Matejisavailable <mat.majla@gmail.com>
2022-11-18 16:14:06 +0100

    dockerizované ale databáza spôsobuje error

---
+++ src/main/java/Mafia/StudentManager/Mapper/StudentMapper.java
@@ -0,0 +1,16 @@
+/*package Mafia.StudentManager.Mapper;
+
+import Mafia.StudentManager.Model.Student;
+import Mafia.StudentManager.DTO.StudentDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentMapper {
+    public Student toEntity(StudentDTO dto){
+        return new Student()
+                .setMeno("Meno");
+
+
+
+    }
+}*/
